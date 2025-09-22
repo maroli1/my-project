9e1358485d7438ea073cdd74101e504cd91a4701
maroli1 <maryamsafaei944@gmail.com>
2025-09-22 13:46:45 -0700

    edite code index.js

--- routes/controller/index.js
+++ routes/controller/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
+// Import functions from usercontrollers.js
 const { getUsers, addUser, updateUser, deleteUser } = require("../usercontrollers");
+
+// روت‌ها
 router.get("/", getUsers);
 router.post("/", addUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.put("/", updateUser);
+router.delete("/", deleteUser);
 
 module.exports = router;
