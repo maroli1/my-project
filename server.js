e875b23f9c1bb37330df50b5ee2e49350c3bd3f8
maroli1 <maryamsafaei944@gmail.com>
2025-09-22 13:41:49 -0700

    edite server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
+
+// Middleware
 const logger = require("./middleware/logger");
-const userRoutes = require("./routes/controller");
+app.use(logger);
 
-app.use(express.json());
-app.use(logger);app.use("/user", userRoutes);
-app.use((err, req, res, next) => {
-    console.error("Error:", err.message);
-    res.status(500).json({ message: "خطای سرور" });
-});
+// Routes
+const userRoutes = require("./routes/controller/index");
+app.use("/user", userRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log("Server is running  port ${PORT}");
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
