a9f10b9de5e2fad7445953dd21f72a1f410ce3b8
maroli1 <maryamsafaei944@gmail.com>
2025-09-22 13:16:11 -0700

    edite adress

--- routes/controller/index.js
+++ routes/controller/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, addUser, updateUser, deleteUser } = require("../../controller/usercontrollers");
-
+const { getUsers, addUser, updateUser, deleteUser } = require("../../usercontrollers");
 router.get("/", getUsers);
 router.post("/", addUser);
 router.put("/:id", updateUser);
