81e87e9d7a7e0458e217b069ca7885012e0ed50d
maroli1 <maryamsafaei944@gmail.com>
2025-09-22 13:09:00 -0700

    edite name index.js

--- routes/controller/index.js
+++ routes/controller/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, addUser, updateUser, deleteUser } = require("../../controllers/usercontrollers");
+const { getUsers, addUser, updateUser, deleteUser } = require("../../controller/usercontrollers");
 
 router.get("/", getUsers);
 router.post("/", addUser);
